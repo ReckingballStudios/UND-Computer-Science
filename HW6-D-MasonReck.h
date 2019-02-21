c6806b19dd1abf265e096e54a3098c5a1755f839
Mason Reck <47192658+ReckingballStudios@users.noreply.github.com>
2019-02-21 00:03:16 -0600

    Add files via upload

---
+++ HW6-D-MasonReck.h
@@ -0,0 +1,24 @@
+/*
+Mason Reck
+CSCI 230
+HW6-D
+2/19/2019
+mason.reck@und.edu
+*/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+void FREE(struct data *blackbox, int size){
+//Free all dynamically allocated memory.
+	//printf("  ...Freeing Memory...\n");
+	for(int k = 1; k < size + 1; k ++){
+		//printf("Freeing %s\t%p\n", blackbox[k].name, &blackbox[k].name);
+		free(blackbox[k].name);
+	}
+			
+	free(blackbox);
+}
+
---
+++ HW6-MasonReck.c
@@ -0,0 +1,52 @@
+/*
+Mason Reck
+CSCI 230
+HW6
+2/19/2019
+mason.reck@und.edu
+*/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+#include "HW6-A-MasonReck.h"
+#include "HW6-B-MasonReck.h"
+#include "HW6-C-MasonReck.h"
+#include "HW6-D-MasonReck.h"
+
+struct data{
+	char *name;
+	long number;
+};
+
+
+int main(int argc, char *(*argv)){
+	if(argc == 1){	//BUG catch
+		printf("You MUST include a name to search for.\n");
+		return 0;
+	}
+
+	//Phase 1: Scanning
+	FILE *dataFile = fopen("HW5.data", "r");
+	int fileSize = SCAN(&dataFile);
+	
+
+	//Phase 2: Loading
+	struct data *blackbox;
+	blackbox = LOAD(dataFile, fileSize);
+
+
+	//Phase 3: Searching
+	for(int z = 1; z < argc; z++)
+		SEARCH(blackbox, *(argv + z), fileSize);
+	
+
+	//Phase 4: Freeing
+	FREE(blackbox, fileSize);
+
+
+	end:
+	fclose(dataFile);
+}
